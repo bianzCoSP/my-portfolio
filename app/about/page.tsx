06059ed63ecf33a191536598ed201418321ec024
bianzCoSP <i_bianzjannkenrick.co@stratpoint.com>
2026-06-25 13:11:41 +0800

    chore(routing): add dynamic routing template to files

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,10 +10,10 @@ export const metadata: Metadata = {
 	},
 };
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+// const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default async function Home() {
-	await delay(2000);
+	// await delay(2000);
 
 	return (
 		<div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<div>
-			<h1>PROJECT</h1>
+			<h1>PROJECT CARD</h1>
 		</div>
 	);
 }
