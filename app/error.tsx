dad6e36311b4c3b929790cea40722591786dd8ef
bianzCoSP <i_bianzjannkenrick.co@stratpoint.com>
2026-06-25 16:35:40 +0800

    feat(error): added reset button to error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
-		<div>
-			<h1>"ERROR ERROR ERROR"</h1>
+		<div className="min-h-screen flex flex-col gap-y-10 items-center justify-center">
+			<h1>An error has occurred.</h1>
+			<Button variant="outline">
+				<Link href="/">Return to Home</Link>
+			</Button>
 		</div>
 	);
 }
