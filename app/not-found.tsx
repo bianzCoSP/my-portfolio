d523908e9643bc060419918c85a475735e2197ba
bianzCoSP <i_bianzjannkenrick.co@stratpoint.com>
2026-07-02 17:10:12 +0800

    feat(not-found): added image to not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export default function NotFoundPage() {
 	return (
 		<div className="flex flex-col items-center justify-center flex-1 w-full gap-y-10">
-			<h1>Resource could not be found.</h1>
+			<Image
+				src="/images/access-denied.png"
+				width={150}
+				height={150}
+				alt="Access Denied Icon"
+				loading="eager"
+			/>
+			<h1>Resource could not be found</h1>
 			<Button variant="outline">
 				<Link href="/">Return to Home</Link>
 			</Button>
